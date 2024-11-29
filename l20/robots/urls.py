95d722ca9976449b51fe2694796db26e2b2af6b4
dnxa <kuridze0@gmail.com>
2024-11-29 12:41:19 +0400

    Add form response

--- l20/robots/urls.py
+++ l20/robots/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import robot_form_request
 
 urlpatterns = [
-    path('robot_form/', robot_form_request, name="robot_form_request")
+    path('', robot_form_request, name="robot_form_request")
 ]
--- l20/robots/views.py
+++ l20/robots/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import RobotForm
 
 # Create your views here.
 def robot_form_request(request):
-    pass
+    if request.method == "GET":
+        form = RobotForm()
+        return render(request, "robot_form.html", {"form" : form}, status=200)
+    elif request.method == "POST":
+        pass
+    else:
+        return HttpResponse("Bad request.", status=400)
