700c5ed886933fb586696e494b538f0426b8dd18
dnxa <kuridze0@gmail.com>
2024-11-29 12:24:53 +0400

    Add form

--- l20/robots/urls.py
+++ l20/robots/urls.py
@@ -1,2 +1,6 @@
+from django.urls import path
+from .views import robot_form_request
+
 urlpatterns = [
+    path('robot_form/', robot_form_request, name="robot_form_request")
 ]
--- l20/robots/views.py
+++ l20/robots/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def robot_form_request(request):
+    pass
