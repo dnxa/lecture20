862b287b7bd2709a2822d5c40eea9f5d852558b8
dnxa <kuridze0@gmail.com>
2024-11-29 12:54:16 +0400

    add make the form work

--- l20/robots/views.py
+++ l20/robots/views.py
@@ -8,6 +8,12 @@ def robot_form_request(request):
         form = RobotForm()
         return render(request, "robot_form.html", {"form" : form}, status=200)
     elif request.method == "POST":
-        pass
+        form = RobotForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # If we saved the form we print a success message.
+            return HttpResponse("Created a robot!", status=200)
+        # If we got here form was not valid so we return 400
+        return HttpResponse("Could not create a robot.", status=400)
     else:
         return HttpResponse("Bad request.", status=400)
